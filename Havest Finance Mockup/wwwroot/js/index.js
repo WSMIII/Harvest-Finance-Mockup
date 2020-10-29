6d69f7d4eaae2e10605225c36c854c7e1d18a74d
William McDuffie <willmcduffie16@gmail.com>
2020-10-29 17:35:06 -0400

    24hr marathon

---
+++ Havest Finance Mockup/wwwroot/js/index.js
@@ -0,0 +1,66 @@
+﻿var curRot = 0;
+
+setInterval(function () {
+    var windmill_blades = document.getElementsByClassName("windmill-blades-img");
+    var cloud_1 = document.getElementsByClassName("cloud-1");
+    var cloud_2 = document.getElementsByClassName("cloud-2");
+    var cloud_3 = document.getElementsByClassName("cloud-3");
+    var cloud_4 = document.getElementsByClassName("cloud-4");
+    var cloud_5 = document.getElementsByClassName("cloud-5");
+    var coin_1 = document.getElementsByClassName("coin-9");
+    var coin_2 = document.getElementsByClassName("coin-10");
+    var coin_3 = document.getElementsByClassName("coin-11");
+    var coin_4 = document.getElementsByClassName("coin-12");
+    var coin_5 = document.getElementsByClassName("coin-13");
+
+    curRot = curRot + 0.5;
+    cloud_1[0].style.left = (cloud_1[0].getBoundingClientRect().x + 0.3) + "px";
+    cloud_2[0].style.left = (cloud_2[0].getBoundingClientRect().x + 0.3) + "px"
+    cloud_3[0].style.left = (cloud_3[0].getBoundingClientRect().x + 0.3) + "px";
+    cloud_4[0].style.left = (cloud_4[0].getBoundingClientRect().x + 0.3) + "px";
+    cloud_5[0].style.left = (cloud_5[0].getBoundingClientRect().x + 0.3) + "px";
+
+    coin_1[0].style.left = (coin_1[0].getBoundingClientRect().x + 0.3) + "px";
+    coin_2[0].style.left = (coin_2[0].getBoundingClientRect().x + 0.3) + "px";
+    coin_3[0].style.left = (coin_3[0].getBoundingClientRect().x + 0.3) + "px";
+    coin_4[0].style.left = (coin_4[0].getBoundingClientRect().x + 0.3) + "px";
+    coin_5[0].style.left = (coin_5[0].getBoundingClientRect().x + 0.3) + "px";
+
+    if (cloud_1[0].getBoundingClientRect().x > document.documentElement.clientWidth) {
+        cloud_1[0].style.left = "-100px";
+    }
+    if (cloud_2[0].getBoundingClientRect().x > document.documentElement.clientWidth) {
+        cloud_2[0].style.left = "-100px";
+    }
+    if (cloud_3[0].getBoundingClientRect().x > document.documentElement.clientWidth) {
+        cloud_3[0].style.left = "-100px";
+    }
+    if (cloud_4[0].getBoundingClientRect().x > document.documentElement.clientWidth) {
+        cloud_4[0].style.left = "-100px";
+    }
+    if (cloud_5[0].getBoundingClientRect().x > document.documentElement.clientWidth) {
+        cloud_5[0].style.left = "-100px";
+    }
+    if (coin_1[0].getBoundingClientRect().x > document.documentElement.clientWidth) {
+        coin_1[0].style.left = "-100px";
+    }
+    if (coin_2[0].getBoundingClientRect().x > document.documentElement.clientWidth) {
+        coin_2[0].style.left = "-100px";
+    }
+    if (coin_3[0].getBoundingClientRect().x > document.documentElement.clientWidth) {
+        coin_3[0].style.left = "-100px";
+    }
+    if (coin_4[0].getBoundingClientRect().x > document.documentElement.clientWidth) {
+        coin_4[0].style.left = "-100px";
+    }
+    if (coin_5[0].getBoundingClientRect().x > document.documentElement.clientWidth) {
+        coin_5[0].style.left = "-100px";
+    }
+
+    if (curRot > 360) {
+        curRot = curRot % 360;
+    }
+
+
+    windmill_blades[0].style.transform = "translateX(-50%) rotate(" + curRot + "deg)";
+}, 20);
---
+++ Havest Finance Mockup/wwwroot/js/site.js
@@ -0,0 +1,60 @@
+﻿var yields = 420764858.13
+var menuState = false;
+
+document.addEventListener("scroll", function () {
+    var header = document.getElementById("site-header");
+
+    if (window.scrollY > 100) {
+        header.style.opacity = "0.0";
+    }
+    else {
+        header.style.opacity = "1.0";
+    }
+});
+
+setInterval(function () {
+    var footer_text = document.getElementsByClassName("footer-text");
+
+    yields = yields + 1.1379;
+    temp = yields.toFixed(2);
+
+    footer_text[0].innerHTML = temp + " of deposits are auto harvesting yields."
+}, 100);
+
+function mobileMenuClick() {
+    var header = document.getElementById("site-header");
+    var home_link_1 = document.getElementsByClassName("home-link-1");
+    var home_link_2 = document.getElementsByClassName("home-link-2");
+    var stake_link = document.getElementsByClassName("stake-link");
+    var faq_link = document.getElementsByClassName("faq-link");
+    var dash_link = document.getElementsByClassName("dash-link");
+    var wiki_link = document.getElementsByClassName("wiki-link");
+    var out_link = document.getElementsByClassName("out-link");
+
+    var body_box = document.getElementById("site-body");
+
+    if (menuState) {
+        header.style.height = "60px";
+        home_link_1[0].style.display = "block";
+        home_link_2[0].style.display = "none";
+        stake_link[0].style.display = "none";
+        faq_link[0].style.display = "none";
+        dash_link[0].style.display = "none";
+        wiki_link[0].style.display = "none";
+        out_link[0].style.display = "none";
+        body_box.style.overflowY = "visible";
+        menuState = false;
+    }
+    else {
+        header.style.height = "100%";
+        home_link_1[0].style.display = "none";
+        home_link_2[0].style.display = "block";
+        stake_link[0].style.display = "block";
+        faq_link[0].style.display = "block";
+        dash_link[0].style.display = "block";
+        wiki_link[0].style.display = "block";
+        out_link[0].style.display = "block";
+        body_box.style.overflowY = "hidden";
+        menuState = true;
+    }
+}
